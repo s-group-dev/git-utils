89cc01dd20c4deadfed405e99e64e0e376ea6f52
Niko Kivelä <niko@tovrleaf.com>
2020-05-28 15:16:57 +0300

    Supress remote add errors if it already exists

--- scripts/remote-merge.sh
+++ scripts/remote-merge.sh
@@ -11,9 +11,7 @@ target_dir="$3"
 project="$(echo $remote_url | sed 's|^.*/\([^/]*\).git$|\1|')"
 
 # add a new remote URL pointing to the separate project thatwe're interested in
-grep -ql "^${project}$" <(git remote) \
-  && git pull -s subtree "${remote_url}" "${branch}" \
-  || git remote add -f "${project}" "${remote_url}"
+git remote add -f "${project}" "${remote_url}" &>/dev/null || true
 
 # merge the project into the local git project. this does not change any files locally, but prepares the next step
 git merge -s ours --no-commit --allow-unrelated-histories "${project}/${branch}"
@@ -23,9 +21,13 @@ git read-tree --prefix="${target_dir}" -u "${project}/${branch}"
 
 # commit the changes to keep them safe
 git commit -F- <<EOF
-[SUBTREE-MERGE] Subtree '${project}' merged to ${target_dir}"
+[SUBTREE-MERGE] Subtree '${project}' merged to ${target_dir}
 
 Merged ${remote_url} and branch ${branch} to directory $target_dir.
 EOF
 
 git remote rm "${project}"
+
+# When a subproject is added, it is not automatically kept in sync with the upstream changes.
+# You will need to update the subproject with the following command:
+# $ git pull -s subtree "${remote_url}" "${branch}"
