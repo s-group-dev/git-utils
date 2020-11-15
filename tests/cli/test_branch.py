1a62e1a6e1c10facabf59e0d507feefdf1beb364
Niko Kivelä <niko@tovrleaf.com>
2020-11-15 23:26:50 +0200

    Improve test coverage for branch command

--- tests/cli/test_branch.py
+++ tests/cli/test_branch.py
@@ -1,4 +1,4 @@
-from unittest.mock import ANY, patch
+from unittest.mock import call, patch
 
 import pytest
 from click.testing import CliRunner
@@ -13,9 +13,50 @@ def runner():
 
 
 @patch.object(BranchService, "list")
-def test_list_merged_is_called_with_branch_name(mock_method, runner):
-    branch = "foo"
-    result = runner.invoke(cli, ["list-merged", "--branch", branch])
+def test_list_merged_is_called_with_params(mock_method, runner):
+    mock_method.return_value = None
 
-    mock_method.assert_called_once_with(branch, ANY)
+    branch = "foo"
+    filter = "filter123"
+    remote = "remote456"
+    params = ["list-merged", "--branch", branch, "--remote", remote, "--filter", filter]
+    result = runner.invoke(cli, params)
+
+    mock_method.assert_called_once_with(branch, [remote, "true", filter])
+
     assert result.exit_code == 0
+    assert result.output.strip() == ""
+
+
+@patch.object(BranchService, "list")
+@patch.object(BranchService, "delete")
+def test_list_merged_and_delete(mock_method_delete, mock_method_list, runner):
+    output1 = "foo\nbar"
+    output2 = ["abc", "def"]
+    mock_method_list.return_value = output1
+    mock_method_delete.side_effect = output2
+
+    branch = "branch123"
+    filter = "filter456"
+    remote = "remote789"
+    params = [
+        "list-merged",
+        "--branch",
+        branch,
+        "--remote",
+        remote,
+        "--filter",
+        filter,
+        "--delete",
+    ]
+    result = runner.invoke(cli, params)
+
+    mock_method_list.assert_called_with(branch, [remote, "true", filter])
+
+    assert mock_method_delete.call_count == 2
+    mock_method_delete.assert_has_calls(
+        [call([remote, x]) for x in output1.splitlines()], any_order=True
+    )
+
+    assert result.exit_code == 0
+    assert result.output.strip() == output1 + "\n" + "\n".join(output2)
